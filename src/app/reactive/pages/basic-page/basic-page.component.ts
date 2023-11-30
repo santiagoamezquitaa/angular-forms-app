10dd8a730dc5d5f79c248ec36f097dfd5a3a9b3e
santiagoamezquitaa <amezquitasantiago0604@gmail.com>
2023-11-29 19:25:12 -0500

    :tada: : Start of application forms

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basic-page.component.html',
+  styles: [],
+})
+export class BasicPageComponent {
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('', [], []),
+  //   price: new FormControl(0, [], []),
+  //   inStorage: new FormControl(0, [], []),
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    if (this.myForm.invalid) return;
+    console.log(this.myForm.value);
+  }
+}
